8c3affea0cfedca00f88408290357fd9c723ebf6
Unbot2313 <126409052+Unbot2313@users.noreply.github.com>
2025-09-13 01:54:53 -0500

    delete register page

--- client/src/features/auth/Auth.tsx
+++ client/src/features/auth/Auth.tsx
@@ -5,24 +5,6 @@ import {
   SignedOut,
   UserButton,
 } from "@clerk/nextjs";
-import AuthContainer from "./components/container";
 export function Auth() {
-  return (
-    <AuthContainer>
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton>
-          <button
-            type="button"
-            className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer"
-          >
-            Sign Up
-          </button>
-        </SignUpButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </AuthContainer>
-  );
+  return <h1>hola</h1>;
 }
